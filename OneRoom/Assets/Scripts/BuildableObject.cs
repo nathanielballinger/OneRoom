bf39eebc6ba61571e0e7afc16ddb79e6eac055cc
nathanielballinger <nathanielballinger@utexas.edu>
2016-12-10 02:54:26 -0600

    BuildableObject changes

--- OneRoom/Assets/Scripts/BuildableObject.cs
+++ OneRoom/Assets/Scripts/BuildableObject.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public abstract class BuildableObject : MonoBehaviour {
 
+    Grid grid;
 	// Use this for initialization
 	public virtual void Start () {
+        grid = GameObject.FindObjectOfType<Grid>();
+        grid.addToGrid(gameObject);
 
 	}
 	
 	// Update is called once per frame
 	public virtual void Update () {
-	
+
 	}
 }
